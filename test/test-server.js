9debe4ebcb4d580dae572b69b082be5c8bebeb6f
viralpatel5757 <viralpatel5757@gmail.com>
2021-03-18 19:42:16 +0000

    changed test file

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes(<h1><p style="color:blue">Welcome to Deloitted application</p></h1>));
+        chai.assert.isTrue(res.text.includes('<h1><p style="color:blue">Welcome to Deloitted application</p></h1>'));
         return done();
       });
 
